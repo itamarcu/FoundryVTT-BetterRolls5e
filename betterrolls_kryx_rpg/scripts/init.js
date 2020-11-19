7c224b758d2b337e98531cbdd383ac81c0b12426
itamar <itamarcu@gmail.com>
2020-11-19 17:00:30 +0200

    rename module name from betterrolls5e to betterrolls_kryx_rpg

---
+++ betterrolls_kryx_rpg/scripts/init.js
@@ -0,0 +1,223 @@
+function i18n(key) {
+	return game.i18n.localize(key);
+}
+
+Hooks.once("init", () => {
+	
+	/**
+	* Register better rolls setting
+	*/
+	game.settings.register("betterrolls_kryx_rpg", "diceEnabled", {
+		name: i18n("br5e.diceEnabled.name"),
+		hint: i18n("br5e.diceEnabled.hint"),
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+	
+	game.settings.register("betterrolls_kryx_rpg", "d20Mode", {
+		name: i18n("br5e.d20Mode.name"),
+		hint: i18n("br5e.d20Mode.hint"),
+		scope: "world",
+		config: true,
+		default: 2,
+		type: Number,
+		choices: {
+			1: i18n("br5e.d20Mode.choices.1"),
+			2: i18n("br5e.d20Mode.choices.2"),
+			3: i18n("br5e.d20Mode.choices.3")
+		}
+	});
+
+	/**
+	* Query roll type in Roll20 style
+	*/
+	game.settings.register("betterrolls_kryx_rpg", "queryAdvantageEnabled", {
+		name: i18n("br5e.queryAdvantageEnabled.name"),
+		hint: i18n("br5e.queryAdvantageEnabled.hint"),
+		scope: "world",
+		config: true,
+		default: false,
+		type: Boolean
+	});
+	
+	/**
+	* Register added roll buttons
+	*/
+	game.settings.register("betterrolls_kryx_rpg", "rollButtonsEnabled", {
+		name: i18n("br5e.rollButtonsEnabled.name"),
+		hint: i18n("br5e.rollButtonsEnabled.hint"),
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+	
+	/**
+	* Register better roll for icon
+	*/
+	game.settings.register("betterrolls_kryx_rpg", "imageButtonEnabled", {
+		name: i18n("br5e.imageButtonEnabled.name"),
+		hint: i18n("br5e.imageButtonEnabled.hint"),
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+	
+	game.settings.register("betterrolls_kryx_rpg", "altSecondaryEnabled", {
+		name: i18n("br5e.altSecondaryEnabled.name"),
+		hint: i18n("br5e.altSecondaryEnabled.hint"),
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+	
+	/**
+	* Register quick roll defaults for description
+	*/
+	game.settings.register("betterrolls_kryx_rpg", "quickDefaultDescriptionEnabled", {
+		name: i18n("br5e.quickDefaultDescriptionEnabled.name"),
+		hint: i18n("br5e.quickDefaultDescriptionEnabled.hint"),
+		scope: "world",
+		config: true,
+		default: false,
+		type: Boolean
+	});
+
+	game.settings.register("betterrolls_kryx_rpg", "defaultRollArt", {
+		name: i18n("br5e.defaultRollArt.name"),
+		hint: i18n("br5e.defaultRollArt.hint"),
+		scope: "world",
+		config: true,
+		default: "actor",
+		type: String,
+		choices: {
+			"actor": i18n("Actor"),
+			"token": i18n("Token")
+		}
+	});
+	
+	/**
+	* Register roll label options
+	*/
+	game.settings.register("betterrolls_kryx_rpg", "rollTitlePlacement", {
+		name: i18n("br5e.rollTitlePlacement.name"),
+		hint: i18n("br5e.rollTitlePlacement.hint"),
+		scope: "world",
+		config: true,
+		default: "1",
+		type: String,
+		choices: {
+			"0": i18n("br5e.damageRollPlacement.choices.0"),
+			"1": i18n("br5e.damageRollPlacement.choices.1")
+		}
+	});
+	
+	const damagePlacementOptions = ["damageTitlePlacement", "damageContextPlacement", "damageRollPlacement"];
+
+	damagePlacementOptions.forEach(placementOption => {
+		game.settings.register("betterrolls_kryx_rpg", placementOption, {
+			name: i18n(`br5e.${placementOption}.name`),
+			hint: i18n(`br5e.${placementOption}.hint`),
+			scope: "world",
+			config: true,
+			default: "1",
+			type: String,
+			choices: {
+				"0": i18n("br5e.damageRollPlacement.choices.0"),
+				"1": i18n("br5e.damageRollPlacement.choices.1"),
+				"2": i18n("br5e.damageRollPlacement.choices.2"),
+				"3": i18n("br5e.damageRollPlacement.choices.3")
+			}
+		});
+	});
+
+	const contextReplacementOptions = ["contextReplacesTitle", "contextReplacesDamage"];
+
+	contextReplacementOptions.forEach(contextOption => {
+		game.settings.register("betterrolls_kryx_rpg", contextOption, {
+			name: i18n(`br5e.${contextOption}.name`),
+			hint: i18n(`br5e.${contextOption}.hint`),
+			scope: "world",
+			config: true,
+			default: false,
+			type: Boolean
+		});
+	});
+	
+	game.settings.register("betterrolls_kryx_rpg", "critBehavior", {
+		name: i18n("br5e.critBehavior.name"),
+		hint: i18n("br5e.critBehavior.hint"),
+		scope: "world",
+		config: true,
+		default: "1",
+		type: String,
+		choices: {
+			"0": i18n("br5e.critBehavior.choices.0"), // No Extra Damage
+			"1": i18n("br5e.critBehavior.choices.1"), // Roll Critical Damage Dice
+			"2": i18n("br5e.critBehavior.choices.2"), // Roll Base Damage, Max Critical
+			"3": i18n("br5e.critBehavior.choices.3"), // Max Base & Critical Damage
+		}
+	});
+	
+	game.settings.register("betterrolls_kryx_rpg", "critString", {
+		name: i18n("br5e.critString.name"),
+		hint: i18n("br5e.critString.hint"),
+		scope: "world",
+		config: true,
+		default: "Crit",
+		type: String
+	});
+	
+	game.settings.register("betterrolls_kryx_rpg", "chatDamageButtonsEnabled", {
+		name: i18n("br5e.chatDamageButtonsEnabled.name"),
+		hint: i18n("br5e.chatDamageButtonsEnabled.hint"),
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+	
+	game.settings.register("betterrolls_kryx_rpg", "playRollSounds", {
+		name: i18n("br5e.playRollSounds.name"),
+		hint: i18n("br5e.playRollSounds.hint"),
+		scope: "world",
+		config: true,
+		default: true,
+		type: Boolean
+	});
+	
+	game.settings.register("betterrolls_kryx_rpg", "hideDC", {
+		name: i18n("br5e.hideDC.name"),
+		hint: i18n("br5e.hideDC.hint"),
+		scope: "world",
+		config: true,
+		default: "0",
+		type: String,
+		choices: {
+			"0": i18n("br5e.hideDC.choices.0"),
+			"1": i18n("br5e.hideDC.choices.1"),
+			"2": i18n("br5e.hideDC.choices.2"),
+		}
+	});
+});
+
+// Modify context menu for damage rolls (they break)
+Hooks.on("getChatLogEntryContext", (html, options) => {
+	let contextDamageLabels = [
+		game.i18n.localize("DND5E.ChatContextDamage"),
+		game.i18n.localize("DND5E.ChatContextHealing"),
+		game.i18n.localize("DND5E.ChatContextDoubleDamage"),
+		game.i18n.localize("DND5E.ChatContextHalfDamage")
+	];
+	
+	for (let i=options.length-1; i>=0; i--) {
+		let option = options[i];
+		if (contextDamageLabels.includes(option.name)) {
+			option.condition = li => canvas.tokens.controlled.length && li.find(".dice-roll").length && !li.find(".red-full").length;
+		}
+	}
+});
---
+++ betterrolls_kryx_rpg/scripts/utils.js
@@ -0,0 +1,17 @@
+export class Utils {
+	static getCharacterLevel(actor) {
+		// Determine character level
+		const level = actor.data.items.reduce((runningTotal, item) => {
+			if ( item.type === "class" ) {
+				const classLevels = parseInt(item.data.levels) || 1;
+				runningTotal += classLevels;
+			}
+			return runningTotal;
+		});
+		return level;
+	}
+
+	static isHalfling(actor) {
+		return getProperty(actor, "data.flags.dnd5e.halflingLucky");
+	}
+}
